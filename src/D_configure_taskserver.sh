6459bd77b5c262866d66f2d0feda5bd6617a0f0d
Mohit Saxena <mohit.saxena@knoldus.com>
2021-09-04 22:26:29 +0530

    Made code Shellcheck complaint

--- src/D_configure_taskserver.sh
+++ src/D_configure_taskserver.sh
@@ -1,197 +1,198 @@
 #!/bin/sh
-mkdir_taskddata() {
-	TASKDDATA=$1
-	output=$(mkdir -p $TASKDDATA)
-	echo $output
+mkdir_taskd_data() {
+	TASKD_DATA=$1
+	output=$(mkdir -p "$TASKD_DATA")
+	echo "$output"
 	# TODO: test whether the folder did not already exist
 }
 
 
-mkdir_taskddata_pki() {
-	TASKDDATA=$1
-	output=$(mkdir -p $TASKDDATA/pki)
-	echo $output
+mkdir_taskd_data_pki() {
+	TASKD_DATA=$1
+	output=$(mkdir -p "$TASKD_DATA"/pki)
+	echo "$output"
 	# TODO: test whether the folder did not already exist
 }
 
 
-initialise_taskserver() {
+initialise_task_server() {
 	output=$(taskd init)
-	echo $output
+	echo "$output"
 }
 
 # TODO: remove duplicate
-make_pki_dir_in_taskddata() {
-	TASKDDATA=$1
-	output=$(mkdir -p $TASKDDATA/pki)
-	echo $output
+make_pki_dir_in_taskd_data() {
+	TASKD_DATA=$1
+	output=$(mkdir -p "$TASKD_DATA"/pki)
+	echo "$output"
 }
 
-copy_pki_content_to_taskddata_dir() {
-	TASKDDATA=$1
-	TASKSERVER_TAR_NAME=$2
-	output=$(cp -r $TASKSERVER_TAR_NAME/pki* $TASKDDATA)
-	echo $output
+copy_pki_content_to_taskd_data_dir() {
+	TASKD_DATA=$1
+	TASK_SERVER_TAR_NAME=$2
+	output=$(cp -r "$TASK_SERVER_TAR_NAME"/pki* "$TASKD_DATA")
+	echo "$output"
 }
 
-# changes the local host in the vars file
-set_local_host_in_vars_file_of_sourcedir() {
+# changes the _local host in the vars file
+set__local_host_in_vars_file_of_source_dir() {
 	IP=$1
 	PORT=$2
-	TASKSERVER_TAR_NAME=$3
-	output=$(sed -i "s|CN=localhost|CN=$IP:$PORT|g" $TASKSERVER_TAR_NAME/pki/vars)
-	echo $output
-	# TODO: write unit test to verify the localhost is changed correctly
+	TASK_SERVER_TAR_NAME=$3
+	output=$(sed -i "s|CN=_localhost|CN=$IP:$PORT|g" "$TASK_SERVER_TAR_NAME"/pki/vars)
+	echo "$output"
+	# TODO: write unit test to verify the _localhost is changed correctly
 }
 
-generate_certificates_in_pki_of_sourcedir() {
-	TASKSERVER_TAR_NAME=$1
-	output=$(cd $TASKSERVER_TAR_NAME/pki && ./generate)
-	echo $output
+generate_certificates_in_pki_of_source_dir() {
+	TASK_SERVER_TAR_NAME=$1
+	output=$(cd "$TASK_SERVER_TAR_NAME"/pki && ./generate)
+	echo "$output"
 }
 
-copy_generated_client_cert_from_pki_in_sourcedir_to_taskddata() {
-	TASKDDATA=$1
-	TASKSERVER_TAR_NAME=$2
-	output=$(cp $TASKSERVER_TAR_NAME/pki/client.cert.pem $TASKDDATA)
-	echo $output
+copy_generated_client_cert_from_pki_in_source_dir_to_taskd_data() {
+	TASKD_DATA=$1
+	TASK_SERVER_TAR_NAME=$2
+	output=$(cp "$TASK_SERVER_TAR_NAME"/pki/client.cert.pem "$TASKD_DATA")
+	echo "$output"
 }
 
-copy_generated_client_key_from_pki_in_sourcedir_to_taskddata() {
-	TASKDDATA=$1
-	TASKSERVER_TAR_NAME=$2
-	output=$(cp $TASKSERVER_TAR_NAME/pki/client.key.pem $TASKDDATA)
-	echo $output
+copy_generated_client_key_from_pki_in_source_dir_to_taskd_data() {
+	TASKD_DATA=$1
+	TASK_SERVER_TAR_NAME=$2
+	output=$(cp "$TASK_SERVER_TAR_NAME"/pki/client.key.pem "$TASKD_DATA")
+	echo "$output"
 }
 
-copy_generated_server_cert_from_pki_in_sourcedir_to_taskddata() {
-	TASKDDATA=$1
-	TASKSERVER_TAR_NAME=$2
-	output=$(cp $TASKSERVER_TAR_NAME/pki/server.cert.pem $TASKDDATA)
-	echo $output
+copy_generated_server_cert_from_pki_in_source_dir_to_taskd_data() {
+	TASKD_DATA=$1
+	TASK_SERVER_TAR_NAME=$2
+	output=$(cp "$TASK_SERVER_TAR_NAME"/pki/server.cert.pem "$TASKD_DATA")
+	echo "$output"
 }
 
-copy_generated_server_key_from_pki_in_sourcedir_to_taskddata() {
-	TASKDDATA=$1
-	TASKSERVER_TAR_NAME=$2
-	output=$(cp $TASKSERVER_TAR_NAME/pki/server.key.pem $TASKDDATA)
-	echo $output
+copy_generated_server_key_from_pki_in_source_dir_to_taskd_data() {
+	TASKD_DATA=$1
+	TASK_SERVER_TAR_NAME=$2
+	output=$(cp "$TASK_SERVER_TAR_NAME"/pki/server.key.pem "$TASKD_DATA")
+	echo "$output"
 }
 
-copy_generated_server_crl_from_pki_in_sourcedir_to_taskddata() {
-	TASKDDATA=$1
-	TASKSERVER_TAR_NAME=$2
-	output=$(cp $TASKSERVER_TAR_NAME/pki/server.crl.pem $TASKDDATA)
-	echo $output
+copy_generated_server_crl_from_pki_in_source_dir_to_taskd_data() {
+	TASKD_DATA=$1
+	TASK_SERVER_TAR_NAME=$2
+	output=$(cp "$TASK_SERVER_TAR_NAME"/pki/server.crl.pem "$TASKD_DATA")
+	echo "$output"
 }
 
-copy_generated_ca_cert_from_pki_in_sourcedir_to_taskddata() {
-	TASKDDATA=$1
-	TASKSERVER_TAR_NAME=$2
-	output=$(cp $TASKSERVER_TAR_NAME/pki/ca.cert.pem $TASKDDATA)
-	echo $output
+copy_generated_ca_cert_from_pki_in_source_dir_to_taskd_data() {
+	TASKD_DATA=$1
+	TASK_SERVER_TAR_NAME=$2
+	output=$(cp "$TASK_SERVER_TAR_NAME"/pki/ca.cert.pem "$TASKD_DATA")
+	echo "$output"
 }
 
 configure_taskd_for_client_cert() {
-	TASKDDATA=$1
-	output=$(taskd config --force client.cert $TASKDDATA/client.cert.pem --data $TASKDDATA)
-	echo $output
+	TASKD_DATA=$1
+	output=$(taskd config --force client.cert "$TASKD_DATA"/client.cert.pem --data "$TASKD_DATA")
+	echo "$output"
 }
 
 configure_taskd_for_client_key() {
-	TASKDDATA=$1
-	output=$(taskd config --force client.key $TASKDDATA/client.key.pem --data $TASKDDATA)
-	echo $output
+	TASKD_DATA=$1
+	output=$(taskd config --force client.key "$TASKD_DATA"/client.key.pem --data "$TASKD_DATA")
+	echo "$output"
 }
 
 configure_taskd_for_server_cert() {
-	TASKDDATA=$1
-	output=$(taskd config --force server.cert $TASKDDATA/server.cert.pem --data $TASKDDATA)
-	echo $output
+	TASKD_DATA=$1
+	output=$(taskd config --force server.cert "$TASKD_DATA"/server.cert.pem --data "$TASKD_DATA")
+	echo "$output"
 }
 
 configure_taskd_for_server_key() {
-	TASKDDATA=$1
-	output=$(taskd config --force server.key $TASKDDATA/server.key.pem --data $TASKDDATA)
-	echo $output
+	TASKD_DATA=$1
+	output=$(taskd config --force server.key "$TASKD_DATA"/server.key.pem --data "$TASKD_DATA")
+	echo "$output"
 }
 
 configure_taskd_for_server_crl() {
-	TASKDDATA=$1
-	output=$(taskd config --force server.crl $TASKDDATA/server.crl.pem --data $TASKDDATA)
-	echo $output
+	TASKD_DATA=$1
+	output=$(taskd config --force server.crl "$TASKD_DATA"/server.crl.pem --data "$TASKD_DATA")
+	echo "$output"
 }
 
 configure_taskd_for_ca_cert() {
-	TASKDDATA=$1
-	output=$(taskd config --force ca.cert $TASKDDATA/ca.cert.pem --data $TASKDDATA)
-	echo $output
+	TASKD_DATA=$1
+	output=$(taskd config --force ca.cert "$TASKD_DATA"/ca.cert.pem --data "$TASKD_DATA")
+	echo "$output"
 }
 
-configure_taskserver_log() {
-	TASKDDATA=$1
+configure_task_server_log() {
+	TASKD_DATA=$1
 	PWD=$2
-	output=$(taskd config --force log $PWD/taskd.log)
-	echo $output
+	output=$(taskd config --force log "$PWD"/taskd.log)
+	echo "$output"
 }
 
-configure_taskserver_pid() {
-	TASKDDATA=$1
+configure_task_server_pid() {
+	TASKD_DATA=$1
 	PWD=$2
-	output=$(taskd config --force pid.file $PWD/taskd.pid)
-	echo $output
+	output=$(taskd config --force pid.file "$PWD"/taskd.pid)
+	echo "$output"
 }
 
-configure_taskserver_ip_and_port() {
+configure_task_server_ip_and_port() {
 	IP=$1
 	PORT=$2
-	output=$(taskd config --force server $IP:$PORT)
-	echo $output
+	output=$(taskd config --force server "$IP":"$PORT")
+	echo "$output"
 }
 
 show_taskd_config() {
 	output=$(taskd config)
-	echo $output
+	echo "$output"
 }
 
 
 start_taskdctl() {
 	output=$(taskdctl start)
-	echo $output
+	echo "$output"
 }
 
 show_running_taskdctl() {
-	output=$(ps -leaf | grep taskd)
-	echo $output
+	output=$(ps -leaf | pgrep taskd)
+	echo "$output"
 }
 
-function swapline_containing_string {
-  local OLD_LINE_PATTERN=$1; shift
-  local NEW_LINE=$1; shift
-  local FILE=$1
-  local NEW=$(echo "${NEW_LINE}" | sed 's/\//\\\//g')
+swapline_containing_string() {
+  _local OLD_LINE_PATTERN="$1"; shift
+  _local NEW_LINE="$1"; shift
+  _local FILE="$1"
+  _local NEW="$(echo "${NEW_LINE}" | sed 's/\//\\\//g')"
   touch "${FILE}"
-  sed -i '/'"${OLD_LINE_PATTERN}"'/{s/.*/'"${NEW}"'/;h};${x;/./{x;q100};x}' "${FILE}"
-  if [[ $? -ne 100 ]] && [[ ${NEW_LINE} != '' ]]
+  # shellcheck disable=SC2154
+  sed -i "/${OLD_LINE_PATTERN}/{s/.*/${NEW}/;h};${x;/./{x;q100};x}" "${FILE}"
+  if [ $? -ne 100 ] && [ "${NEW_LINE}" != '' ]
   then
     echo "${NEW_LINE}" >> "${FILE}"
   fi
 }
 
 modify_taskd_service_file(){
-	taskddata=$1
-	echo "taskddata=$taskddata" >&2
+	taskd_data=$1
+	echo "taskd_data=$taskd_data" >&2
 	
 	linux_username=$(whoami)
 	echo "linux_username=$linux_username" >&2
 	linux_group=$linux_username
 	echo "linux_group=$linux_group" >&2
 	
-	swapline_containing_string "ExecStart=" "ExecStart=/usr/local/bin/taskd server --data $taskddata" "src/taskd.service"
+	swapline_containing_string "ExecStart=" "ExecStart=/usr/_local/bin/taskd server --data $taskd_data" "src/taskd.service"
 	swapline_containing_string "User=" "User=$linux_username" "src/taskd.service"
 	swapline_containing_string "Group=" "Group=$linux_group" "src/taskd.service"
-	swapline_containing_string "WorkingDirectory=" "WorkingDirectory=$taskddata" "src/taskd.service"
-	echo $output
+	swapline_containing_string "WorkingDirectory=" "WorkingDirectory=$taskd_data" "src/taskd.service"
+	echo "$output"
 	# TODO: unit test
 	# TODO: move swapline function to helper.sh
 }
@@ -227,96 +228,96 @@ enable_starting_taskd_server_at_boot() {
 	echo "output=$output" >&2
 }
 
-add_organisation_to_taskserver() {
-	TASKDDATA=$1
+add_organisation_to_task_server() {
+	TASKD_DATA=$1
 	TW_ORGANISATION=$2
-	output=$(taskd add org $TW_ORGANISATION --data $TASKDDATA)
-	echo $output
+	output=$(taskd add org "$TW_ORGANISATION" --data "$TASKD_DATA")
+	echo "$output"
 }
 
-add_user_to_taskserver() {
-	TASKDDATA=$1
+add_user_to_task_server() {
+	TASKD_DATA=$1
 	TW_ORGANISATION=$2
 	TW_USERNAME=$3
-	output=$(taskd add user $TW_ORGANISATION $TW_USERNAME --data $TASKDDATA)
-	echo $output
+	output=$(taskd add user "$TW_ORGANISATION" "$TW_USERNAME" --data "$TASKD_DATA")
+	echo "$output"
 }
 
 generate_user_certificate() {
-	TASKSERVER_TAR_NAME=$1
+	TASK_SERVER_TAR_NAME=$1
 	TW_USERNAME=$2
-	output=$(cd $TASKSERVER_TAR_NAME/pki && ./generate.client $TW_USERNAME)
-	echo $output
+	output=$(cd "$TASK_SERVER_TAR_NAME"/pki && ./generate.client "$TW_USERNAME")
+	echo "$output"
 	# TODO: make username First into a variable
 }
 
 copy_user_certificate_to_dot_task_dir() {
 	LINUX_USERNAME=$1
-	TASKSERVER_TAR_NAME=$2
-	output=$(cp $TASKSERVER_TAR_NAME/pki/$TW_USERNAME.cert.pem /home/$LINUX_USERNAME/.task)
-	echo $output
+	TASK_SERVER_TAR_NAME=$2
+	output=$(cp "$TASK_SERVER_TAR_NAME"/pki/"$TW_USERNAME".cert.pem /home/"$LINUX_USERNAME"/.task)
+	echo "$output"
 	# TODO: change the ~ to some variable of the user path
 }
 
 copy_user_certificate_key_to_dot_task_dir() {
 	LINUX_USERNAME=$1
-	TASKSERVER_TAR_NAME=$2
-	output=$(cp $TASKSERVER_TAR_NAME/pki/$TW_USERNAME.key.pem /home/$LINUX_USERNAME/.task)
-	echo $output
+	TASK_SERVER_TAR_NAME=$2
+	output=$(cp "$TASK_SERVER_TAR_NAME"/pki/"$TW_USERNAME".key.pem /home/"$LINUX_USERNAME"/.task)
+	echo "$output"
 	# TODO: change the ~ to some variable of the user path
 }
 
 copy_ca_cert_to_dot_task_dir() {
 	LINUX_USERNAME=$1
-	TASKSERVER_TAR_NAME=$2
-	output=$(cp $TASKSERVER_TAR_NAME/pki/ca.cert.pem /home/$LINUX_USERNAME/.task)
-	echo $output
+	TASK_SERVER_TAR_NAME=$2
+	output=$(cp "$TASK_SERVER_TAR_NAME"/pki/ca.cert.pem /home/"$LINUX_USERNAME"/.task)
+	echo "$output"
 	# TODO: change the ~ to some variable of the user path
 }
 
-configure_taskserver_config_user_certificate() {
+configure_task_server_config_user_certificate() {
 	LINUX_USERNAME=$1
 	TW_USERNAME=$2
-	output=$(yes | task config taskd.certificate -- /home/$LINUX_USERNAME/.task/$TW_USERNAME.cert.pem)
-	echo $output
+	output=$(yes | task config taskd.certificate -- /home/"$LINUX_USERNAME"/.task/"$TW_USERNAME".cert.pem)
+	echo "$output"
 	# TODO: change the ~ to some variable of the user path
 }
 
 # 20 
-configure_taskserver_config_user_key() {
+configure_task_server_config_user_key() {
 	LINUX_USERNAME=$1
 	TW_USERNAME=$2
-	output=$(yes | task config taskd.key -- /home/$LINUX_USERNAME/.task/$TW_USERNAME.key.pem)
-	echo $output
+	output=$(yes | task config taskd.key -- /home/"$LINUX_USERNAME"/.task/"$TW_USERNAME".key.pem)
+	echo "$output"
 	# TODO: change the ~ to some variable of the user path
 }
 
-configure_taskserver_config_ca_cert() {
-	output=$(yes | task config taskd.ca -- /home/$LINUX_USERNAME/.task/ca.cert.pem)
-	echo $output
+configure_task_server_config_ca_cert() {
+	output=$(yes | task config taskd.ca -- /home/"$LINUX_USERNAME"/.task/ca.cert.pem)
+	echo "$output"
 	# TODO: change the ~ to some variable of the user path
 }
 
-configure_taskserver_config_ip_and_port() {
+configure_task_server_config_ip_and_port() {
 	IP=$1
 	PORT=$2
 	#output=$(yes | sudo task config taskd.server -- 0.0.0.0:53589)
-	output=$(yes | task config taskd.server -- $IP:$PORT)
-	echo $output
+	output=$(yes | task config taskd.server -- "$IP":"$PORT")
+	echo "$output"
 	# TODO: make server and port into variable
 }
 
 # 21
-set_taskserver_credentials() {
+set_task_server_credentials() {
 	TW_ORGANISATION=$1
 	TW_USERNAME=$2
 	KEY=$3
 	echo "in function KEY=$KEY"
-	output=$(yes | task config taskd.credentials -- $TW_ORGANISATION/$TW_USERNAME/$KEY)
-	echo $output
+	output=$(yes | task config taskd.credentials -- "$TW_ORGANISATION"/"$TW_USERNAME"/"$KEY")
+	echo "$output"
 }
 
-first_taskserver_sync_init() {
+first_task_server_sync_init() {
 	output=$(yes | task sync init)
-	echo $output
+	echo "$output"
 }
--- src/helper.sh
+++ src/helper.sh
@@ -1,29 +1,34 @@
 #!/bin/sh
 write_to_log() {
 
-	# read incoming arguments
-	dest_dir=$1
-	dest_file=$2
-	dest_path=$dest_dir$dest_file
-	
-	shift # eat first argument
-	shift # eat second argument
-	
-	# create empty file (overwrite previous content if it exists)
-	> $dest_path
-	
-	# append remaining args to file
-	for i in "$*"; do
-		cat <<< "$i" >> $dest_path
-	done
+  # read incoming arguments
+  dest_dir=$1
+  dest_file=$2
+  dest_path=$dest_dir$dest_file
+
+  shift # eat first argument
+  shift # eat second argument
+
+  # create empty file (overwrite previous content if it exists)
+  # shellcheck disable=SC2188
+  >"$dest_path"
+
+  # append remaining args to file
+  # shellcheck disable=SC2066
+  for i in "$*"; do
+    # shellcheck disable=SC2039
+    cat <<<"$i" >>"$dest_path"
+  done
 }
 
 remove_logs() {
-	rm -r src/logs/*
+  rm -r src/logs/*
 }
 
 read_user_key_from_log() {
-	value=$(<src/logs/D_26_add_user_to_taskserver.txt)
-	RESULT=${value:14:36}
-	echo $RESULT
+  # shellcheck disable=SC2039
+  value=$(<src/logs/D_26_add_user_to_taskserver.txt)
+  # shellcheck disable=SC2039
+  RESULT=${value:14:36}
+  echo "$RESULT"
 }
